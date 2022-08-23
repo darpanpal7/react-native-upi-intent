3d452a1dd9d63323b9a08000a2b756bd8ad17ee1
Darpan Pal <whodarpan@Darpans-MacBook-Pro.local>
2022-08-23 20:21:05 +0530

    Push to github

--- ios/UpiIntent.mm
+++ ios/UpiIntent.mm
@@ -1,11 +1,13 @@
 #import "UpiIntent.h"
+#include <objc/runtime.h>
+#import <Foundation/Foundation.h>
 
 #ifdef RCT_NEW_ARCH_ENABLED
 #import "RNUpiIntentSpec.h"
 #endif
 
 @implementation UpiIntent
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE(UpiIntent)
 
 // Example method
 // See // https://reactnative.dev/docs/native-modules-ios
@@ -17,6 +19,32 @@ RCT_REMAP_METHOD(multiply,
   NSNumber *result = @(a * b);
 
   resolve(result);
+}
+
+RCT_REMAP_METHOD(canOpenUpi,
+                 payload:(NSString*)payload
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+{
+  NSURL *appURL = [NSURL URLWithString: payload];
+  BOOL status = [[UIApplication sharedApplication] canOpenURL:appURL];
+  resolve(status?@true:@false);
+}
+
+
+RCT_REMAP_METHOD(openUpiApp,
+                 urlString:(NSString*)payload
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+{
+  NSURL *appURL = [NSURL URLWithString:payload];
+  if (appURL && [[UIApplication sharedApplication] canOpenURL:appURL]) {
+    dispatch_async(dispatch_get_main_queue(), ^{[[UIApplication sharedApplication] openURL:appURL options:@{} completionHandler:^(BOOL success) {
+        if (success) resolve(@true);
+        else resolve(@false);
+    }];
+  });
+  } else resolve(@false);
 }
 
 // Don't compile this code when we build for the old architecture.
--- src/index.tsx
+++ src/index.tsx
@@ -27,6 +27,55 @@ type AppItem = {
   packageName: string;
 };
 
+type PackageNameEnum =
+  | 'com.phonepe.app'
+  | 'net.one97.paytm'
+  | 'com.google.android.apps.nbu.paisa.user'
+  | 'in.org.npci.upiapp';
+
+const packageNameList = [
+  'com.phonepe.app',
+  'net.one97.paytm',
+  'com.google.android.apps.nbu.paisa.user',
+  'in.org.npci.upiapp',
+];
+
+const linkPayPrefixMap = {
+  'com.phonepe.app': 'phonepe://pay?',
+  'net.one97.paytm': 'paytmmp://upi/pay?',
+  'com.google.android.apps.nbu.paisa.user': 'tez://upi/pay?',
+  'in.org.npci.upiapp': 'bhim://upi?',
+};
+
+const linkMandatePrefixMap = {
+  'com.phonepe.app': 'phonepe://mandate?',
+  'net.one97.paytm': 'paytmmp://upi/mandate?',
+  'com.google.android.apps.nbu.paisa.user': 'tez://upi/mandate?',
+  'in.org.npci.upiapp': 'bhim://upi?',
+};
+
+const linkPayMap = {
+  'com.phonepe.app':
+    'phonepe://pay?pa=paytm-68550775@paytm&pn=Gullak%20Money&mc=5499&tid=PYTM20220412304077016706&tr=20220412304077016706&am=10&cu=INR',
+  'net.one97.paytm':
+    'paytmmp://upi/pay?pa=paytm-68550775@paytm&pn=Gullak%20Money&mc=5499&tid=PYTM20220412304077016706&tr=20220412304077016706&am=10&cu=INR',
+  'com.google.android.apps.nbu.paisa.user':
+    'tez://upi/pay?pa=paytm-68550775@paytm&pn=Gullak%20Money&mc=5499&tid=PYTM20220412304077016706&tr=20220412304077016706&am=10&cu=INR',
+  'in.org.npci.upiapp':
+    'bhim://upi?pa=paytm-68550775@paytm&pn=Gullak%20Money&mc=5499&tid=PYTM20220412304077016706&tr=20220412304077016706&am=10&cu=INR',
+};
+
+const linkMandateApp = {
+  'com.phonepe.app':
+    'phonepe://mandate?pa=paytm-68550775@paytm&pn=Gullak&mc=5499&tid=PYTM20220812352667308803&tr=PAYTMSUBS20220812352667308803&tn=Amount%20to%20be%20paid%20now%20is%20Rs%201.00&am=100.00&mam=1.00&cu=INR&mode=00&purpose=14&orgid=000000&validitystart=12082022&validityend=31122031&amrule=MAX&recur=ASPRESENTED&mn=Subscription%20for%20Gullak',
+  'net.one97.paytm':
+    'paytmmp://upi/mandate?pa=paytm-68550775@paytm&pn=Gullak&mc=5499&tid=PYTM20220812352667308803&tr=PAYTMSUBS20220812352667308803&tn=Amount%20to%20be%20paid%20now%20is%20Rs%201.00&am=100.00&mam=1.00&cu=INR&mode=00&purpose=14&orgid=000000&validitystart=12082022&validityend=31122031&amrule=MAX&recur=ASPRESENTED&mn=Subscription%20for%20Gullak',
+  'com.google.android.apps.nbu.paisa.user':
+    'tez://upi/mandate?pa=paytm-68550775@paytm&pn=Gullak&mc=5499&tid=PYTM20220812352667308803&tr=PAYTMSUBS20220812352667308803&tn=Amount%20to%20be%20paid%20now%20is%20Rs%201.00&am=100.00&mam=1.00&cu=INR&mode=00&purpose=14&orgid=000000&validitystart=12082022&validityend=31122031&amrule=MAX&recur=ASPRESENTED&mn=Subscription%20for%20Gullak',
+  'in.org.npci.upiapp':
+    'bhim://upi?pa=paytm-68550775@paytm&pn=Gullak&mc=5499&tid=PYTM20220812352667308803&tr=PAYTMSUBS20220812352667308803&tn=Amount%20to%20be%20paid%20now%20is%20Rs%201.00&am=100.00&mam=1.00&cu=INR&mode=00&purpose=14&orgid=000000&validitystart=12082022&validityend=31122031&amrule=MAX&recur=ASPRESENTED&mn=Subscription%20for%20Gullak',
+};
+
 export async function getUpiApps(
   type?: 'PAY' | 'MANDATE'
 ): Promise<Array<AppItem>> {
@@ -34,7 +83,16 @@ export async function getUpiApps(
     return UpiIntent.getUpiApps(
       type === 'MANDATE' ? MANDATE_SCHEME : DEFAULT_SCHEME
     );
-  return [];
+  const resultList = [];
+  for (const [key, value] of Object.entries(
+    type === 'MANDATE' ? linkMandateApp : linkPayMap
+  )) {
+    try {
+      if (await UpiIntent.canOpenUpi(value))
+        resultList.push({ packageName: key });
+    } catch (err) {}
+  }
+  return resultList;
 }
 
 export async function invokeIntent(
@@ -45,5 +103,12 @@ export async function invokeIntent(
     return packageName
       ? UpiIntent.invokeIntent(deeplink, packageName)
       : UpiIntent.invokeIntent(deeplink, 'null');
-  return true;
+
+  if (!packageName || !packageNameList.includes(packageName)) return false;
+  deeplink = deeplink.startsWith('upi://mandate?')
+    ? linkMandatePrefixMap[packageName as PackageNameEnum] +
+      deeplink.replace('upi://mandate?', '')
+    : linkPayPrefixMap[packageName as PackageNameEnum] +
+      deeplink.replace('upi://pay?', '');
+  return await UpiIntent.openUpiApp(deeplink);
 }
