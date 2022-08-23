e4ce985a483ecc8dcecde01a584dc9e855c6aa86
Darpan Pal <whodarpan@Darpans-MacBook-Pro.local>
2022-08-23 20:13:29 +0530

    IOS side complete

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text, Button } from 'react-native';
-import { multiply, getUpiApps } from 'react-native-upi-intent';
+import { multiply, invokeIntent } from 'react-native-upi-intent';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
@@ -10,9 +10,12 @@ export default function App() {
     multiply(3, 7).then(setResult);
   }, []);
 
+  const deepLink =
+    'upi://mandate?pa=paytm-68550775@paytm&pn=Gullak&mc=5499&tid=PYTM20220812352667308803&tr=PAYTMSUBS20220812352667308803&tn=Amount%20to%20be%20paid%20now%20is%20Rs%201.00&am=100.00&mam=1.00&cu=INR&mode=00&purpose=14&orgid=000000&validitystart=12082022&validityend=31122031&amrule=MAX&recur=ASPRESENTED&mn=Subscription%20for%20Gullak';
+
   const handlePress = async () => {
     try {
-      const res = await getUpiApps('MANDATE');
+      const res = await invokeIntent(deepLink, 'com.phonepe.app');
       console.info(res);
     } catch (err) {
       console.error('ERROR', err);
