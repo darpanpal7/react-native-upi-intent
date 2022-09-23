59e50d609377ddd982e25fbe449b2b3dedc80967
Darpan Pal <whodarpan@Darpans-MacBook-Pro.local>
2022-09-23 15:37:17 +0530

    Updated files & packages

--- src/index.tsx
+++ src/index.tsx
@@ -23,73 +23,39 @@ export function multiply(a: number, b: number): Promise<number> {
 
 const MANDATE_SCHEME = 'upi://mandate';
 const DEFAULT_SCHEME = 'upi://pay';
-type AppItem = {
-  packageName: string;
-};
 
-type PackageNameEnum =
-  | 'com.phonepe.app'
-  | 'net.one97.paytm'
-  | 'com.google.android.apps.nbu.paisa.user'
-  | 'in.org.npci.upiapp';
-
-const packageNameList = [
-  'com.phonepe.app',
-  'net.one97.paytm',
-  'com.google.android.apps.nbu.paisa.user',
-  'in.org.npci.upiapp',
-];
-
-const linkPayPrefixMap = {
-  'com.phonepe.app': 'phonepe://pay?',
-  'net.one97.paytm': 'paytmmp://upi/pay?',
-  'com.google.android.apps.nbu.paisa.user': 'tez://upi/pay?',
-  'in.org.npci.upiapp': 'bhim://upi?',
-};
-
-const linkMandatePrefixMap = {
-  'com.phonepe.app': 'phonepe://mandate?',
-  'net.one97.paytm': 'paytmmp://upi/mandate?',
-  'com.google.android.apps.nbu.paisa.user': 'tez://upi/mandate?',
-  'in.org.npci.upiapp': 'bhim://upi?',
-};
-
-const linkPayMap = {
-  'com.phonepe.app':
-    'phonepe://pay?pa=paytm-68550775@paytm&pn=Gullak%20Money&mc=5499&tid=PYTM20220412304077016706&tr=20220412304077016706&am=10&cu=INR',
-  'net.one97.paytm':
-    'paytmmp://upi/pay?pa=paytm-68550775@paytm&pn=Gullak%20Money&mc=5499&tid=PYTM20220412304077016706&tr=20220412304077016706&am=10&cu=INR',
-  'com.google.android.apps.nbu.paisa.user':
-    'tez://upi/pay?pa=paytm-68550775@paytm&pn=Gullak%20Money&mc=5499&tid=PYTM20220412304077016706&tr=20220412304077016706&am=10&cu=INR',
-  'in.org.npci.upiapp':
-    'bhim://upi?pa=paytm-68550775@paytm&pn=Gullak%20Money&mc=5499&tid=PYTM20220412304077016706&tr=20220412304077016706&am=10&cu=INR',
-};
-
-const linkMandateApp = {
-  'com.phonepe.app':
-    'phonepe://mandate?pa=paytm-68550775@paytm&pn=Gullak&mc=5499&tid=PYTM20220812352667308803&tr=PAYTMSUBS20220812352667308803&tn=Amount%20to%20be%20paid%20now%20is%20Rs%201.00&am=100.00&mam=1.00&cu=INR&mode=00&purpose=14&orgid=000000&validitystart=12082022&validityend=31122031&amrule=MAX&recur=ASPRESENTED&mn=Subscription%20for%20Gullak',
-  'net.one97.paytm':
-    'paytmmp://upi/mandate?pa=paytm-68550775@paytm&pn=Gullak&mc=5499&tid=PYTM20220812352667308803&tr=PAYTMSUBS20220812352667308803&tn=Amount%20to%20be%20paid%20now%20is%20Rs%201.00&am=100.00&mam=1.00&cu=INR&mode=00&purpose=14&orgid=000000&validitystart=12082022&validityend=31122031&amrule=MAX&recur=ASPRESENTED&mn=Subscription%20for%20Gullak',
-  'com.google.android.apps.nbu.paisa.user':
-    'tez://upi/mandate?pa=paytm-68550775@paytm&pn=Gullak&mc=5499&tid=PYTM20220812352667308803&tr=PAYTMSUBS20220812352667308803&tn=Amount%20to%20be%20paid%20now%20is%20Rs%201.00&am=100.00&mam=1.00&cu=INR&mode=00&purpose=14&orgid=000000&validitystart=12082022&validityend=31122031&amrule=MAX&recur=ASPRESENTED&mn=Subscription%20for%20Gullak',
-  'in.org.npci.upiapp':
-    'bhim://upi?pa=paytm-68550775@paytm&pn=Gullak&mc=5499&tid=PYTM20220812352667308803&tr=PAYTMSUBS20220812352667308803&tn=Amount%20to%20be%20paid%20now%20is%20Rs%201.00&am=100.00&mam=1.00&cu=INR&mode=00&purpose=14&orgid=000000&validitystart=12082022&validityend=31122031&amrule=MAX&recur=ASPRESENTED&mn=Subscription%20for%20Gullak',
-};
+const mandateLink =
+  'upi://mandate?pa=paytm-68550775@paytm&pn=Gullak&mc=5499&tid=PYTM20220519323795870130&tr=PAYTMSUBS20220519323795870130&tn=Amount%20to%20be%20paid%20now%20is%20Rs%2015.00&am=15.00&mam=15.00&cu=INR&mode=00&purpose=14&orgid=000000&validitystart=19052022&validityend=06052023&amrule=MAX&recur=ASPRESENTED&mn=Subscription%20for%20Gullak';
+const payLink =
+  'upi://pay?pa=paytm-68550775@paytm&pn=Gullak%20Money&mc=5499&tid=PYTM20220412304077016706&tr=20220412304077016706&am=10&cu=INR';
 
 export async function getUpiApps(
+  apps: Array<{ packageName: string; appScheme: string }>,
   type?: 'PAY' | 'MANDATE'
-): Promise<Array<AppItem>> {
-  if (Platform.OS === 'android')
-    return UpiIntent.getUpiApps(
+): Promise<Array<{ packageName: string }>> {
+  if (Platform.OS === 'android') {
+    const appsAvailable = await UpiIntent.getUpiApps(
       type === 'MANDATE' ? MANDATE_SCHEME : DEFAULT_SCHEME
     );
+    const resultList = [];
+    for (const { packageName } of apps) {
+      if (
+        appsAvailable.some(
+          (e: { packageName: string }) => e.packageName === packageName
+        )
+      )
+        resultList.push({ packageName });
+    }
+
+    return resultList;
+  }
   const resultList = [];
-  for (const [key, value] of Object.entries(
-    type === 'MANDATE' ? linkMandateApp : linkPayMap
-  )) {
+  for (const { packageName, appScheme } of apps) {
     try {
-      if (await UpiIntent.canOpenUpi(value))
-        resultList.push({ packageName: key });
+      const testDeeplink = type === 'MANDATE' ? mandateLink : payLink;
+      const specificDeeplink = createDeeplink(testDeeplink, appScheme);
+      if (await UpiIntent.canOpenUpi(specificDeeplink))
+        resultList.push({ packageName });
     } catch (err) {}
   }
   return resultList;
@@ -97,18 +63,28 @@ export async function getUpiApps(
 
 export async function invokeIntent(
   deeplink: string,
-  packageName?: string
+  name?: string
 ): Promise<boolean> {
   if (Platform.OS === 'android')
-    return packageName
-      ? UpiIntent.invokeIntent(deeplink, packageName)
+    return name
+      ? UpiIntent.invokeIntent(deeplink, name)
       : UpiIntent.invokeIntent(deeplink, 'null');
 
-  if (!packageName || !packageNameList.includes(packageName)) return false;
-  deeplink = deeplink.startsWith('upi://mandate?')
-    ? linkMandatePrefixMap[packageName as PackageNameEnum] +
-      deeplink.replace('upi://mandate?', '')
-    : linkPayPrefixMap[packageName as PackageNameEnum] +
-      deeplink.replace('upi://pay?', '');
+  if (!name) return false;
+  deeplink = createDeeplink(deeplink, name);
+  if (!deeplink) return false;
+  // console.log(await UpiIntent.canOpenUpi(deeplink));
   return await UpiIntent.openUpiApp(deeplink);
+}
+
+function createDeeplink(deeplink: string, name: string) {
+  if (Platform.OS === 'android') return deeplink;
+  const scheme = deeplink.startsWith(MANDATE_SCHEME)
+    ? MANDATE_SCHEME
+    : deeplink.startsWith(DEFAULT_SCHEME)
+    ? DEFAULT_SCHEME
+    : null;
+  if (!scheme) return '';
+  deeplink = deeplink.replace(scheme, name);
+  return deeplink;
 }
